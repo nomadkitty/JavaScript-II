fcaeb8a3ed9bf2dab66001491d4e050f31580b52
Jojo Zhang <jojozhang0327@gmail.com>
2019-07-23 18:51:54 -0400

    some stretch goals

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -89,8 +89,9 @@ console.log(ticketPriceTotal);
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
 
-// Problem 1
-
+// Problem 1: sort the first name alphabetically 
+let newFirstName = runners.map((arrItem) => arrItem.first_name)
+console.log(newFirstName.sort());
 // Problem 2
 
 // Problem 3
