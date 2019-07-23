d00d2c43a16dbd73561f9fd90653c7f1bd24b0cf
Jojo Zhang <jojozhang0327@gmail.com>
2019-07-23 18:02:51 -0400

    finish MVP

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,6 +1,16 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+const x = 5; 
+  function add() {
+    let y = 6;
+      function nested (){
+        let z = 7;
+        return x + y + z;
+      }
+    return nested();
+  }
 
+console.log(add());
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
